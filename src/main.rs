1fa03d3bbd9f46e902bb3d55f6f569c8964872a8
Ometeor-Zheero-OMZ <sicsempertyrannis98@gmail.com>
2025-02-28 22:02:25 +0900

    全部出す！

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,34 @@
+// # Copyright 2024 Your Organization
+// # Licensed under the Apache License, Version 2.0 (the "License");
+// # you may not use this file except in compliance with the License.
+// # You may obtain a copy of the License at
+// #
+// #     http://www.apache.org/licenses/LICENSE-2.0
+// #
+// # Modifications to this file must include:
+// # - The contributor's name.
+// # - A description of the changes made.
+// #
+// # Unless required by applicable law or agreed to in writing, software
+// # distributed under the License is distributed on an "AS IS" BASIS,
+// # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// # See the License for the specific language governing permissions and
+// # limitations under the License.
+
+
 use std::{process, time::{Duration, SystemTime}};
 
 use anyhow::{Context, Result};
 use crossterm::terminal;
 use clap::Parser;
 use matrix::waterfall::{Axis, MatrixWaterfall};
-use rand::SeedableRng;
-use rand_xoshiro::Xoshiro256PlusPlus;
 
 mod cli;
 mod matrix;
 mod utils;
 
 use cli::{command_line::{Args, TextColor, TextLanguage}, print::clear_screen};
-use utils::color::Color;
+use utils::{color::Color, rand::SplitMixXoshiro256Rng};
 
 
 fn main() -> Result<()> {
@@ -41,7 +57,6 @@ fn main() -> Result<()> {
     if args.random {
         (r, g, b) = TextColor::random_rgb();
         language_set = TextLanguage::random_language();
-        // println!("Random: ({}, {}, {})", r, g, b);
     }
 
     // if -R option entered
@@ -86,11 +101,11 @@ fn main() -> Result<()> {
     }
 
     // random number
-    let seed = SystemTime::now().duration_since(SystemTime::UNIX_EPOCH).expect("time to have passed since UNIX_EPOCH").as_micros() as u64;
-    let mut rand = Xoshiro256PlusPlus::seed_from_u64(seed);
+    let seed = SystemTime::now().duration_since(SystemTime::UNIX_EPOCH).expect("Generate seed with UNIX_EPOCH").as_nanos() as u64;
+    let mut rng = SplitMixXoshiro256Rng::new(seed);
 
     // waterfall
-    let (width, height) = terminal::size().context("determine terminal size")?;
+    let (width, height) = terminal::size().context("Determine terminal size: width and height")?;
 
     let mut waterfall = match axis {
         Axis::Y => MatrixWaterfall::new_column(width, height, base_color, threshold_density, is_rainbow_mode),
@@ -109,7 +124,7 @@ fn main() -> Result<()> {
     // start
     loop {
         waterfall.render_matrix(&mut stdout, text_size_code, &axis)?;
-        waterfall.update_direction(language_set, &mut rand);
+        waterfall.update_direction(language_set, &mut rng);
         std::thread::sleep(Duration::from_millis(velocity));
     }
 }
